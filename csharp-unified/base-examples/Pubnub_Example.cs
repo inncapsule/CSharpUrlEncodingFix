7802a18deb94bb6c456f0af348a372085f7a805e
Leonardo Redmond <leonaro.kyj@gmail.com>
2012-08-05 23:56:37 +0800

    Testing for add a new repository for csharp-unified

---
+++ csharp-unified/base-examples/Pubnub_Example.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace PubNub_Messaging
+{
+    public class Pubnub_Example
+    {
+        static public void Main()
+        {
+            Pubnub pubnub = new Pubnub(
+            "demo",
+            "demo",
+            "",
+            false
+            );
+            string channel = "my/channel";
+            string message = "Pubnub API Usage Example";
+
+            pubnub.PropertyChanged += new PropertyChangedEventHandler(Pubnub_PropertyChanged);
+
+            Console.WriteLine("UUID -> " + pubnub.generateGUID());
+
+            pubnub.publish(channel, message);
+
+            pubnub.history(channel, 10);
+
+            pubnub.time();
+
+            pubnub.subscribe(channel);
+
+            Console.ReadKey();
+        }
+
+        static void Pubnub_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "History":
+                    foreach (object history_message in ((Pubnub)sender).History)
+                    {
+                        Console.WriteLine("History Message: ");
+                        Dictionary<string, object> _messageHistory = (Dictionary<string, object>)(history_message);
+                        Console.WriteLine(_messageHistory["text"]);
+                    }
+                    break;
+                case "Publish":
+                    Console.WriteLine(
+                    "Publish Success: " + ((Pubnub)sender).Publish[0].ToString() +
+                    "\nPublish Info: " + ((Pubnub)sender).Publish[1].ToString()
+                    );
+                    break;
+                case "Time":
+                    Console.WriteLine("Time:  " + ((Pubnub)sender).Time[0].ToString());
+                    break;
+                case "ReturnMessage":
+                    Dictionary<string, object> _message = (Dictionary<string, object>)(((Pubnub)sender).ReturnMessage);
+                    Console.WriteLine("Received Message -> '" + _message["text"] + "'");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ csharp-unified/base-unit-tests/WhenSubscribedToAChannel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace PubNub_Messaging.Tests
+{
+    [TestClass]
+    public class WhenSubscribedToAChannel
+    {
+        [TestMethod]
+        public void ThenItShouldReturnReceivedMessage()
+        {
+            Pubnub pubnub = new Pubnub(
+                "demo",
+                "demo",
+                "",
+                false
+            );
+            string channel = "my/channel";
+
+            pubnub.PropertyChanged += new PropertyChangedEventHandler(Pubnub_PropertyChanged);
+
+            pubnub.subscribe(channel);
+        }
+
+        static void Pubnub_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Dictionary<string, object> _message = (Dictionary<string, object>)(((Pubnub)sender).ReturnMessage);
+            Assert.IsNotNull(_message["text"]);
+        }
+    }
+}
